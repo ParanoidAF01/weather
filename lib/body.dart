13f8b40a4bfe8f0fac5a8e8d278dc57079afba8d
ParanoidAF <error.anonymous.04@gmail.com>
2024-08-28 00:00:04 +0530

    27

--- lib/body.dart
+++ lib/body.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomeBody extends StatelessWidget {
@@ -5,6 +7,100 @@ class HomeBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Card();
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 250,
+            child: Card(
+              elevation: 10,
+              borderOnForeground: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "33°C",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+                  ),
+                  Icon(
+                    Icons.cloud,
+                    size: 72,
+                  ),
+                  Text(
+                    "Rain",
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Weather Forecast",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 150,
+            child: Card(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                  ),
+                  Card(
+                    elevation: 10,
+                    borderOnForeground: true,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "33°C",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 14),
+                        ),
+                        Icon(
+                          Icons.cloud,
+                          size: 14,
+                        ),
+                        Text(
+                          "Rain",
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
